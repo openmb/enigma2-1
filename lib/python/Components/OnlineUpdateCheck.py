c59af57bf1f6e5592d0e013961701444ab91e78d
rossi2000 <rossi_2000@hotmail.com>
2016-02-20 20:15:15 +0000

    [onlineupdatecheck] remove some code

--- lib/python/Components/OnlineUpdateCheck.py
+++ lib/python/Components/OnlineUpdateCheck.py
@@ -33,14 +33,7 @@ class FeedsStatusCheck:
 
 	def getFeedSatus(self):
 		status = '1'
-		trafficLight = 'stable'
-		if getImageType() != 'developer':
-			status = '0'
-			config.softwareupdate.updateisunstable.setValue(status)
-			return 'stable'
-
 		trafficLight = 'unknown'
-
 		if about.getIfConfig('eth0').has_key('addr') or about.getIfConfig('eth1').has_key('addr') or about.getIfConfig('wlan0').has_key('addr') or about.getIfConfig('ra0').has_key('addr'):
 			try:
 				print '[OnlineVersionCheck] Checking feeds state'
